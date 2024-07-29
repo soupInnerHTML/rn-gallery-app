03c358568843fb3a5d5732777252d23741097220
soupInnerHTML <andrey.da.scrim@gmail.com>
2024-07-29 07:47:40 +0500

    Update ImageGrid.flashList.tsx

--- src/components/ImageGrid/ImageGrid.flashList.tsx
+++ src/components/ImageGrid/ImageGrid.flashList.tsx
@@ -1,16 +1,7 @@
 import {ListRenderItem} from '@shopify/flash-list';
 import {IImage} from '../../types/image';
-import {LogBox} from 'react-native';
 import {ImageOptimized} from '../ImageOptimized';
 import React from 'react';
-
-// Я игнорирую эти сообщения поскольку не использую estimatedItemSize в Masonry, тк у всех изображений разная высота
-LogBox.ignoreLogs(['estimatedItemSize FlashList prop is not defined']);
-console.warn = (...args) => {
-  if (args[0].includes('estimatedItemSize FlashList prop is not defined')) {
-    return;
-  }
-};
 
 export const imageGridRenderItem: ListRenderItem<IImage> = ({item}) => (
   <ImageOptimized {...{image: item}} />
